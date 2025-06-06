ad33750e618c37944886af52d03ef10577267084
rumbella <lapo.ristori@gmail.com>
2025-06-06 17:14:34 +0200

    Update ArtworkInfo.tsx

--- src/components/ArtworkInfo.tsx
+++ src/components/ArtworkInfo.tsx
@@ -16,10 +16,9 @@ const ArtworkInfo = () => {
       
       {/* Menu Button - Bottom Right */}
       <div className="fixed bottom-8 right-8">
-        <button className="flex items-center text-white text-sm font-light tracking-wider hover:opacity-80 transition-opacity">
-          MENU
-          <Menu className="ml-2 w-4 h-4" />
-        </button>
+           <button className="text-sm font-light tracking-wider hover:opacity-80 transition-opacity">
+            GALLERY
+          </button>
       </div>
     </>
   );
