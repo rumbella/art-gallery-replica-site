1369555c8a1af8d98e8fe9fd4df04525ea253173
rumbella <lapo.ristori@gmail.com>
2025-06-06 17:06:05 +0200

    Update ArtworkInfo.tsx

--- src/components/ArtworkInfo.tsx
+++ src/components/ArtworkInfo.tsx
@@ -5,22 +5,14 @@ import { Menu } from 'lucide-react';
 const ArtworkInfo = () => {
   return (
     <>
-      {/* Year and Number - Top Left */}
-      <div className="fixed top-1/2 left-8 text-white">
-        <div className="text-2xl font-light">2021</div>
-        <div className="text-4xl font-light">01</div>
-      </div>
+    
       
       {/* Copyright - Top Right */}
       <div className="fixed top-1/2 right-8 text-white text-sm opacity-60">
-        ©2024
+        ©2025
       </div>
       
-      {/* Artwork Title - Bottom Left */}
-      <div className="fixed bottom-8 left-8 text-white">
-        <div className="text-xs opacity-60 mb-1">PAINTING</div>
-        <div className="text-lg font-light tracking-wider">"YOUNG"</div>
-      </div>
+      
       
       {/* Menu Button - Bottom Right */}
       <div className="fixed bottom-8 right-8">
