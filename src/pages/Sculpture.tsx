7c80d2dcd554a668ec8b2407d0ecac577aca182b
rumbella <lapo.ristori@gmail.com>
2025-08-31 16:43:18 +0200

    Update Sculpture.tsx

--- src/pages/Sculpture.tsx
+++ src/pages/Sculpture.tsx
@@ -8,10 +8,10 @@ import {
 
 const sculptures = [
   { id: 1, src: "https://res.cloudinary.com/thinkdigital/image/upload/v1756651070/giacomo/tFdPgitxQ6sfC6HnLSmnan5C4_1.jpg", alt: "Sculpture 1" },
-  { id: 2, src: "https://images.unsplash.com/photo-1618160702438-9b02ab6515c9?w=400&h=400&fit=crop", alt: "Sculpture 2" },
-  { id: 3, src: "https://images.unsplash.com/photo-1582562124811-c09040d0a901?w=600&h=600&fit=crop", alt: "Sculpture 3" },
-  { id: 4, src: "https://images.unsplash.com/photo-1618160702438-9b02ab6515c9?w=400&h=400&fit=crop", alt: "Sculpture 4" },
-  { id: 5, src: "https://images.unsplash.com/photo-1721322800607-8c38375eef04?w=400&h=400&fit=crop", alt: "Sculpture 5" },
+  { id: 2, src: "https://res.cloudinary.com/thinkdigital/image/upload/v1756651073/giacomo/3WZTlsJaYlPj3lWgScdi6gTiw_1.jpg", alt: "Sculpture 2" },
+  { id: 3, src: "https://res.cloudinary.com/thinkdigital/image/upload/v1756651070/giacomo/8IsKshDpc376UAyKxYIKY9m1E0_1.jpg", alt: "Sculpture 3" },
+  { id: 4, src: "https://res.cloudinary.com/thinkdigital/image/upload/v1756651070/giacomo/EtQsdQPjVZuiB9PBy1nBYAMnp8_1.jpg", alt: "Sculpture 4" },
+  { id: 5, src: "https://res.cloudinary.com/thinkdigital/image/upload/v1756651073/giacomo/7jWaJ7geij3fuRD3XSAmp6zsOcc_1.jpgp", alt: "Sculpture 5" },
 ];
 
 const Sculpture = () => {
